b0f4a7c45dfb13787d6cb4f5f8175d1d2050c6dc
InnaTovstik <kitimail69@gmail.com>
2021-08-12 19:24:02 +0300

    Add .gitignore. Add code

---
+++ src/com/company/StringLinkedList.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public interface StringLinkedList {
+
+    void addFirst(String s);
+
+    void add(String s, int index);
+
+    void addLast(String s);
+
+    String getFirst();
+
+    String get(int i);
+
+    String getLast();
+
+    String removeFirst();
+
+    String remove(int i);
+
+    String removeLast();
+
+    int size();
+
+}
---
+++ src/com/company/StringLinkedListImpl.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class StringLinkedListImpl implements StringLinkedList {
+
+    private Node first;
+    private Node last;
+
+    private void addFirst() {
+
+        addFirst();
+    }
+
+
+
+}
+
